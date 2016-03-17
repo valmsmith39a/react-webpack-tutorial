258bf8368b853e6221458231bbf5fbb6d78425d8
valmsmith39a <valmsmith39a@gmail.com>
2016-03-16 20:26:36 -0700

    Added another component

---
+++ src/client/app/AnotherComponent.jsx
@@ -0,0 +1,16 @@
+import React from 'react'; 
+
+class AnotherComponent extends React.Component {
+  constructor(props) {
+    super(props); 
+  }
+  render() {
+    return (
+      <div> 
+        Another Component!!!!!!
+      </div>
+    );
+  }
+}
+
+export default AnotherComponent;
--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {render} from 'react-dom';
 import AwesomeComponent from './AwesomeComponent.jsx';
+import AnotherComponent from './AnotherComponent.jsx';
 
 class App extends React.Component {
   render () {
@@ -8,6 +9,7 @@ class App extends React.Component {
       <div>
         <p> HELLO React!!!!! </p>
         <AwesomeComponent />
+        <AnotherComponent />
       </div>
       );
   }
